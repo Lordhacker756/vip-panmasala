5be08b4b7eaaec43ab3b036e4a7a641cbd2cbc3b
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2023-11-27 00:50:24 +0530

    Added homepage banner

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -13,6 +13,10 @@ import "./style.css";
 import { Pagination, Navigation } from "swiper/modules";
 
 export default function Carousel() {
+  const loaderImage = () => {
+    return `https://placehold.co/2000x1900/png`;
+  };
+
   return (
     <>
       <Swiper
@@ -22,18 +26,43 @@ export default function Carousel() {
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
       >
         <SwiperSlide>
-          <Image src={"/assets/c1.jpeg"} alt="random" fill={true} />
+          <Image
+            src={"/assets/c1.jpeg"}
+            alt="random"
+            fill={true}
+            loading={"eager"}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={"/assets/c2.jpeg"} alt="random" fill={true} />
+          <Image
+            src={"/assets/c2.jpeg"}
+            alt="random"
+            fill={true}
+            loading={"eager"}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={"/assets/c3.jpeg"} alt="random" fill={true} />
+          <Image
+            src={"/assets/c3.jpeg"}
+            alt="random"
+            fill={true}
+            loading={"lazy"}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={"/assets/c4.jpeg"} alt="random" fill={true} />
+          <Image
+            src={"/assets/c4.jpeg"}
+            alt="random"
+            fill={true}
+            loading={"lazy"}
+          />
         </SwiperSlide>
       </Swiper>
     </>

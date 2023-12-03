53988ecf83a4610a611642e51a46eb101b81cbab
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2023-12-03 16:48:14 +0530

    FEAT: Added why acq and pdts

--- app/acquiesce/page.tsx
+++ app/acquiesce/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Image from "next/image";
+import Product from "../../public/assets/Product.png";
+import Card from "@/components/Card/Card";
 function Acquiesce() {
   return (
     <div>
@@ -24,8 +26,7 @@ function Acquiesce() {
                 CHOOSE SUBSCRIPTION & DELIVERY PLAN
               </h2>
               <p className="leading-relaxed text-base">
-                Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                taxidermy. Gastropub indxgo juice poutine.
+                Select Quantity, Subscription Period and Shipping Frequency
               </p>
             </div>
             <div className="sm:w-32 order-first sm:order-none sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full text-theme_gold bg-gray-800 flex-shrink-0">
@@ -60,6 +61,36 @@ function Acquiesce() {
           </div>
         </div>
       </section>
+
+      <section className="border-2 border-theme_gold rounded-lg mx-10 bg-gray-800 flex justify-center items-center p-5">
+        <Image
+          src={Product}
+          alt="Product"
+          className="sm:w-[15vw] w-[60vw] mx-10"
+        />
+        <div className="w-[40%]">
+          <h2 className="text-2xl text-theme_gold font-bold">Why Acquiesce?</h2>
+          <div className="text-xl mt-4 leading-loose">
+            - Enjoy free delivery on every order <br />
+            - Get 2X Reward Points
+            <br />
+            - Subscribe for 1, 3, 6 or 12 Months. <br />
+            - Shipped every – Week, Fortnight or Month
+            <br /> - Cancel or Pause, any time.
+          </div>
+        </div>
+      </section>
+
+      <h2 className="text-2xl text-theme_gold font-bold mt-20 text-center">
+        Our Premium Products
+      </h2>
+      <div className="flex flex-wrap justify-center gap-5 mx-10">
+        <Card img={"pdt1"} />
+        <Card img={"pdt2"} />
+        <Card img={"pdt3"} />
+        <Card img={"pdt5"} />
+        <Card img={"pdt6"} />
+      </div>
     </div>
   );
 }

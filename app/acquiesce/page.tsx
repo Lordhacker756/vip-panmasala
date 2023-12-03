043c5e2eff366cee48711ff902d6b58f27728962
Krishna Chaitanya Kommera <eshkon.krishna@gmail.com>
2023-12-03 14:23:05 +0530

    added acquiesce page UI

--- app/acquiesce/page.tsx
+++ app/acquiesce/page.tsx
@@ -1,7 +1,67 @@
 import React from "react";
 
 function Acquiesce() {
-  return <div>Acquiesce</div>;
+  return (
+    <div>
+      <section className="text-gray-400  body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-theme_gold bg-gray-800 flex-shrink-0">
+              <p className="text-5xl  ">1</p>
+            </div>
+            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-theme_gold text-lg title-font font-medium mb-2">
+                Select A Product
+              </h2>
+              <p className="leading-relaxed text-base">
+                Pick favorite pack size and tap Subscribe Now
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-theme_gold text-lg title-font font-medium mb-2">
+                CHOOSE SUBSCRIPTION & DELIVERY PLAN
+              </h2>
+              <p className="leading-relaxed text-base">
+                Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine.
+              </p>
+            </div>
+            <div className="sm:w-32 order-first sm:order-none sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full text-theme_gold bg-gray-800 flex-shrink-0">
+              <p className="text-5xl  ">2</p>
+            </div>
+          </div>
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-theme_gold bg-gray-800 flex-shrink-0">
+              <p className="text-5xl  ">3</p>
+            </div>
+            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-theme_gold text-lg title-font font-medium mb-2">
+                DOORSTEP DELIVERY
+              </h2>
+              <p className="leading-relaxed text-base">
+                Enter Payment Details and Enjoy Doorstep Delivery
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-800 sm:flex-row flex-col">
+            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-theme_gold text-lg title-font font-medium mb-2">
+                ENJOY ADDITIONAL BENEFITS
+              </h2>
+              <p className="leading-relaxed text-base">
+                Get Free Delivery and 2 X Rewards
+              </p>
+            </div>
+            <div className="sm:w-32 order-first sm:order-none sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full text-theme_gold bg-gray-800 flex-shrink-0">
+              <p className="text-5xl  ">4</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Acquiesce;
